6a33c6c6a9c7d368dce22fbd927eeeb9bf60d1f5
dduartevla <deboraduarte@ice.ufjf.br>
2023-07-10 13:22:57 -0300

    Cria classe Transação

---
+++ src/main/java/br/ufuf/dcc193/debora/trabalhofinal/trabalhofinal/TrabalhofinalApplication.java
@@ -0,0 +1,13 @@
+package br.ufuf.dcc193.debora.trabalhofinal.trabalhofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhofinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufuf/dcc193/debora/trabalhofinal/trabalhofinal/TrabalhofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufuf.dcc193.debora.trabalhofinal.trabalhofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
